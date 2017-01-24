8d65571c3b73b4f47782b2f3ba0c22f24f578be0
Denne Reed <denne.reed@gmail.com>
2017-01-24 14:13:00 -0800

    In projects app, added abstract to admin list view.

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Project
 
-# Register your models here.
+
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['priority', 'slug', 'title', 'theme', 'status', 'published', 'date_initiated', 'date_published']
+    list_display = ['priority', 'slug', 'title', 'abstract', 'theme', 'status', 'published', 'date_initiated', 'date_published']
     list_filter = ['published', 'status', 'theme', 'target_journal', 'publication_type']
     search_fields = ['id', 'slug', 'title', 'citation', 'authors', 'abstract']
     list_display_links = ['slug', 'title']
