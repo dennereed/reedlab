d2bee105f286e45162e63d9bfdb124c2beea341a
Denne Reed <denne.reed@gmail.com>
2017-01-26 22:28:34 -0800

    Updated microid admin listview

--- microid/admin.py
+++ microid/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from models import Character
 
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ['cid', 'charname', 'unit', 'notes', 'chartype', 'mandatory', 'element', 'disabled']
+    list_filter = ['chartype', 'element', 'disabled', 'mandatory']
+
+
 admin.site.register(Character)
