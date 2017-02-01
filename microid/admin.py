07e46a762755ff62f020be724a5e1073ddac7f9d
Denne Reed <denne.reed@gmail.com>
2017-02-01 09:36:59 -0800

    Updates to microid models and admin.

--- microid/admin.py
+++ microid/admin.py
@@ -1,11 +1,31 @@
 from django.contrib import admin
-from models import Character, CharacterState
+from models import Character, CharacterState, Item, Description, Species, ItemImage
 
 
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ['cid', 'charname', 'unit', 'notes', 'chartype', 'mandatory', 'element', 'disabled']
-    list_filter = ['chartype', 'element', 'disabled', 'mandatory']
+    list_display = ['cid', 'char_name', 'unit', 'notes', 'char_type', 'mandatory', 'element', 'disabled']
+    list_filter = ['char_type', 'element', 'disabled', 'mandatory']
 
+
+class DescriptionInline(admin.TabularInline):
+    model = Description
+    ordering = ['character', 'char_state']
+    extra = 1
+    classes = ['collapse']
+
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
+    extra =1
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'item_name', 'notes', 'species', 'taxonomic_order']
+    list_filter = ['taxonomic_order', 'collection_code']
+    inlines = [ItemImageInline, DescriptionInline]
+    search_fields = ['item_name', 'item_wording', 'notes', 'collection_code', 'taxonomic_order']
 
 admin.site.register(Character, CharacterAdmin)
 admin.site.register(CharacterState)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Species)
--- microid/models.py
+++ microid/models.py
@@ -7,40 +7,40 @@ ELEMENT_CHOICES = (('U', 'Upper Jaw'), ('L', 'Lower Jaw'), ('B', 'Both'))
 
 class Character(models.Model):
     cid = models.AutoField(primary_key=True)
-    charname = models.CharField(max_length=50, default='', null=False)
+    char_name = models.CharField(max_length=50, default='', null=False)
     unit = models.CharField(max_length=5, null=True)
     notes = models.TextField(null=True)
-    chartype = models.CharField(max_length=5, default='UM', null=False, choices=CHARTYPE_CHOICES)
+    char_type = models.CharField(max_length=5, default='UM', null=False, choices=CHARTYPE_CHOICES)
     mandatory = models.NullBooleanField(default=False)
-    multistatetype = models.IntegerField(default=1, null=False)
+    multistate_type = models.IntegerField(default=1, null=False)
     reliability = models.IntegerField(default=5)
     availability = models.IntegerField(default=5)
     fuzziness = models.DecimalField(decimal_places=5, max_digits=20, default=0, null=False)
-    fuzzinessispercent = models.NullBooleanField(default=False)
-    keystates = models.CharField(max_length=5, null=True)
-    charheading = models.IntegerField(null=True)
-    headinglink = models.IntegerField(null=True)
-    charwording = models.TextField(null=True)
-    charwording2 = models.TextField(null=True)
-    unitisprefix = models.NullBooleanField(default=False)
-    formatstring = models.TextField(null=True)
-    paragraphlink = models.IntegerField(null=True)
-    sentencelink = models.IntegerField(null=True)
-    commalink = models.IntegerField(null=True)
-    speciallink = models.IntegerField(null=True)
-    specialelement = models.IntegerField(null=True)
+    fuzzinessis_percent = models.NullBooleanField(default=False)
+    key_states = models.CharField(max_length=5, null=True)
+    char_heading = models.IntegerField(null=True)
+    heading_link = models.IntegerField(null=True)
+    char_wording = models.TextField(null=True)
+    char_wording2 = models.TextField(null=True)
+    unit_is_prefix = models.NullBooleanField(default=False)
+    format_string = models.TextField(null=True)
+    paragraph_link = models.IntegerField(null=True)
+    sentence_link = models.IntegerField(null=True)
+    comma_link = models.IntegerField(null=True)
+    special_link = models.IntegerField(null=True)
+    special_element = models.IntegerField(null=True)
     usecomma2 = models.NullBooleanField(default=False)
-    omitfinalcomma = models.NullBooleanField(default=False)
-    omitvalues = models.NullBooleanField(default=False)
+    omit_final_comma = models.NullBooleanField(default=False)
+    omit_values = models.NullBooleanField(default=False)
     emphasize = models.NullBooleanField(default=False)
-    omitperiod = models.NullBooleanField(default=False)
-    numstates = models.IntegerField(default=2, null=True)
-    charref = models.IntegerField(null=True)
+    omit_period = models.NullBooleanField(default=False)
+    num_states = models.IntegerField(default=2, null=True)
+    char_ref = models.IntegerField(null=True)
     element = models.CharField(max_length=4,  default='U', null=True, choices=ELEMENT_CHOICES)
     disabled = models.NullBooleanField(default=False, null=False)
 
     def __unicode__(self):
-        return self.charname
+        return self.char_name
 
 
 class CharacterState(models.Model):
@@ -53,7 +53,7 @@ class CharacterState(models.Model):
     implicit = models.BooleanField(default=False)
     use_edit = models.NullBooleanField(default=True)
     use_identify = models.NullBooleanField(default=False)
-    use_descr = models.NullBooleanField(default=False)
+    use_description = models.NullBooleanField(default=False)
     use_phylo = models.NullBooleanField(default=False)
     use_other = models.NullBooleanField(default=False)
     min_value = models.DecimalField(decimal_places=5, max_digits=20, default=0, null=False)
@@ -62,3 +62,60 @@ class CharacterState(models.Model):
     def __unicode__(self):
         return self.char_state_name
 
+
+class Species(models.Model):
+    genus_name = models.CharField(max_length=50, null=False)
+    trivial_name =  models.CharField(max_length=50, null=False)
+    common_name = models.CharField(max_length=50, null=True)
+    type_citation = models.TextField(null=True)
+    type_location_id = models.IntegerField(null=True)
+    type_specimen_no = models.CharField(max_length=100, null=True)
+    generic_type = models.NullBooleanField()
+    genus_citation = models.TextField(null=True)
+    taxonomic_order = models.CharField(max_length=50, null=True)
+    taxonomic_family = models.CharField(max_length=50, null=True)
+    taxonomic_subfamily = models.CharField(max_length=50, null=True)
+    notes = models.TextField(null=True)
+    extant = models.NullBooleanField()
+
+    def __unicode__(self):
+        return self.genus_name + ' ' + self.trivial_name
+
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100, null=False)
+    item_wording = models.CharField(max_length=100, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    abundance = models.IntegerField(null=True, blank=True)
+    collection_unit = models.CharField(max_length=50, null=True, blank=True)
+    lit_ref = models.CharField(max_length=100, null=True, blank=True)
+    lit_key = models.IntegerField(null=True, blank=True)
+    lit_ref_detail = models.TextField(null=True, blank=True)
+    collection_code = models.CharField(max_length=15, null=True, blank=True)
+    specimen_no = models.CharField(max_length=20, null=True, blank=True)
+    species = models.ForeignKey(Species)
+    taxonomic_order = models.CharField(max_length=50, null=True, blank=True)
+    disabled = models.NullBooleanField(default=False)
+
+    def __unicode__(self):
+        return self.item_name
+
+
+class Description(models.Model):
+    item = models.ForeignKey(Item)
+    character = models.ForeignKey(Character)
+    modifier = models.CharField(max_length=10, null=True, blank=True)
+    char_state = models.CharField(max_length=10, null=True, blank=True)
+    x = models.DecimalField(decimal_places=5, max_digits=20, default=0, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    sequence = models.IntegerField(default=0, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.item.__unicode__()+'['+self.character.__unicode__()+', '+self.char_state+']'
+
+
+class ItemImage(models.Model):
+    item = models.ForeignKey(Item)
+    image = models.ImageField(upload_to='Item_Images')
+    description = models.TextField(null=True, blank=True)
