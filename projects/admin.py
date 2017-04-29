885aff7141f5941bedd10250f41c72d13e1a44e9
Denne Reed <denne.reed@gmail.com>
2017-04-29 05:14:09 -0700

    Updates to projects.admin.py allow listview editing of priority

--- projects/admin.py
+++ projects/admin.py
@@ -3,10 +3,11 @@ from .models import Project
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['priority', 'slug', 'title', 'abstract', 'theme', 'status', 'published', 'date_initiated', 'date_published']
+    list_display = ['priority', 'slug', 'title', 'abstract', 'theme', 'status', 'published', 'date_initiated',
+                    'date_published']
     list_filter = ['published', 'status', 'theme', 'target_journal', 'publication_type']
     search_fields = ['id', 'slug', 'title', 'citation', 'authors', 'abstract']
     list_display_links = ['slug', 'title']
-    list_editable = ['theme', 'status', 'published']
+    list_editable = ['priority', 'theme', 'status', 'published']
 
 admin.site.register(Project, ProjectAdmin)
